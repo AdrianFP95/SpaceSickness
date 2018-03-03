b48990804196dbfffdc483d2ff8776a9c476ae6f
BraisMorgade <36987428+BraisMorgade@users.noreply.github.com>
2018-03-03 22:17:46 +0100

    CambioGravedad
    Externalicé algunos valores y restringí el cambio de gravedad a cuando el jugador esté quieto de forma que no pueda cambiar la dirección en caída

--- Assets/Scripts/CambioGravedad.cs
+++ Assets/Scripts/CambioGravedad.cs
@@ -3,23 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CambioGravedad : MonoBehaviour {
+	public float gravedad;//fuerza con la que atrae los objetos. Valor positivo.
+	Rigidbody2D rb;
+	public float margenmovimiento;//velocidad máxima a la que se puede cambiar la direccion de la gravedad. Valor positivo.
+	//Disponibilidad de cambio de gravedad en cada una de las 4 direcciones, en el juego completo dependen de las variables guardadas en cada sala.
+	public bool gravedadArriba;
+	public bool gravedadAbajo;
+	public bool gravedadIzquierda;
+	public bool gravedadDerecha;
+
+
+	void Awake(){
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
 
 
 	void Update () {
 
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			Physics2D.gravity = new Vector2 (0f, 4f);
+		if (Mathf.Abs(rb.velocity.x) <= margenmovimiento && Mathf.Abs(rb.velocity.y)<= margenmovimiento)
+		{
+			CambiarGravedad ();
 		}
-		if (Input.GetKey (KeyCode.DownArrow)) {
-			Physics2D.gravity = new Vector2 (0f, -4f);
-		}
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			Physics2D.gravity = new Vector2 (4f, 0f);
-		}
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			Physics2D.gravity = new Vector2 (-4f, 0f);
-		}
+	}
 
-
+	void CambiarGravedad(){
+		
+		if (Input.GetKey (KeyCode.UpArrow) && gravedadArriba) Physics2D.gravity = new Vector2 (0f, gravedad);
+		
+		if (Input.GetKey (KeyCode.DownArrow) && gravedadAbajo) Physics2D.gravity = new Vector2 (0f, -gravedad);
+		
+		if (Input.GetKey (KeyCode.RightArrow) && gravedadDerecha) Physics2D.gravity = new Vector2 (gravedad, 0f);
+		
+		if (Input.GetKey (KeyCode.LeftArrow) && gravedadIzquierda) Physics2D.gravity = new Vector2 (-gravedad, 0f);
+		
 	}
 }
