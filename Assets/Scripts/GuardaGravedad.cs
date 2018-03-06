24b359f6213e3e4459941f205035bfcd18c67153
oscarpb99 <oscarped@ucm.es>
2018-03-07 00:34:57 +0100

    Intento con mi parte

---
+++ Assets/Scripts/GuardaGravedad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardaGravedad : MonoBehaviour {
+	public float gravedad;
+	Rigidbody2D rb;
+
+
+	void Awake(){
+		rb =GetComponent<Rigidbody2D> ();
+
+		}
+	// Use this for initialization
+	void Start () {
+		
+		rb.gravityScale=1f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider2D other)
+	{
+		rb.gravityScale =gravedad;
+	}
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+	public float vel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate (vel * Time.deltaTime, 0, 0);
+		}
+	}
+}
